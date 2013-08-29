f49a43b3eaaf63eace56ac94409959a5628d9ad9
Will Fuqua <wafuqua@gmail.com>
2013-08-28 21:01:36 -0400

    add multithreading demo

--- master/pibooth.py
+++ master/pibooth.py
@@ -1,6 +1,7 @@
 import datetime, time, os
 from subprocess import call
 from time import sleep
+import threading
 
 def timestamped(fname, fmt='%Y_%m_%d_%H%M%S.{fname}'):
     return datetime.datetime.now().strftime(fmt).format(fname=fname)
@@ -19,6 +20,27 @@ def takePicture(delay = 0, preview = False):
     args.append("-op " + str(212))
     call([' '.join(args)], shell=True)
 
+
+
+# Threading demo. This method is invoked in another thread
+def notifications():
+    sleep(1)
+    print("3!")
+    sleep(1)
+    print("2!")
+    sleep(1)
+    print("1!")
+    sleep(1)
+
+# Kick off the thread
+notifyThread = threading.Thread(target=notifications)
+notifyThread.daemon = True
+notifyThread.start()
+
+# This is our main thread, fake invoking the raspistill binary
+print("Invoking arduino")
+sleep(4)
+print("Taking picture!")
 
 """
 # TODO: count down of 3 2 1 should also start at around 5 seconds ?
